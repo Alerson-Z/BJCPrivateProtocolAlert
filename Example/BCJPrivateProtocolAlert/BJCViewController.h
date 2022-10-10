01bce461df142c6f643860c53ae6f1752576834e
Alerson <zhsworkmail@163.com>
2022-10-10 15:12:49 +0800

    创建cocopod 项目

---
+++ Example/BCJPrivateProtocolAlert/BJCViewController.h
@@ -0,0 +1,13 @@
+//
+//  BJCViewController.h
+//  BCJPrivateProtocolAlert
+//
+//  Created by oauth2 on 10/10/2022.
+//  Copyright (c) 2022 oauth2. All rights reserved.
+//
+
+@import UIKit;
+
+@interface BJCViewController : UIViewController
+
+@end
